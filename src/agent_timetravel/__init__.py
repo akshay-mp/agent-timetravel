1683921a361fad27835fcc96e182094e5621ab4a
akshay-mp <akshaymp.1810@gmail.com>
2026-08-25 21:33:58 +0530

    Release 0.2.2

--- src/agent_timetravel/__init__.py
+++ src/agent_timetravel/__init__.py
@@ -10,7 +10,7 @@ from agent_timetravel.agents import AgentDefinition, TimeTravel, TimeTravelConte
 from agent_timetravel.checkpoint import checkpoint
 from agent_timetravel.tool_intercept import tool
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 timetravel = TimeTravel()
 __all__ = [
     "AgentDefinition",
